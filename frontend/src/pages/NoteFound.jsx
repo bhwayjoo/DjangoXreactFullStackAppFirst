b8b2e601151fc7a291c602eaeb054f361c656244
abdelkhalek hanbel <116838826+bhwayjoo@users.noreply.github.com>
2024-06-21 17:56:32 +0100

    Delete frontend directory

--- frontend/src/pages/NoteFound.jsx
+++
@@ -1,9 +0,0 @@
-function NoteFound() {
-  return (
-    <div>
-      <h1>404 Not Found</h1>
-      <p>The page you re looking for doesn t not exit!</p>
-    </div>
-  );
-}
-export default NoteFound;
