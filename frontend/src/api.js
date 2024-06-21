74e51defa3d0aa61647f52ee8254270cf397a32e
abdelkhalek hanbel <116838826+bhwayjoo@users.noreply.github.com>
2024-06-21 18:28:37 +0100

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
 
-const apiUrl = "https://58e8ea98-f35f-4fc0-b33a-1a04a5b77aa3-dev.e1-eu-north-azure.choreoapis.dev/reackxdjagno/backend-pwb/v1";
+const apiUrl = "/choreo-apis/reackxdjagno/backend-pwb/v1";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
